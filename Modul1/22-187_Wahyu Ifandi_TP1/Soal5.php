f43b1f33e990b7a7765d20f2ec756d559ce7cffe
Ifandi-wahyu <wifandi6@gmail.com>
2025-10-08 12:03:59 +0700

    Add files via upload

---
+++ Modul1/22-187_Wahyu Ifandi_TP1/Soal5.php
@@ -0,0 +1,9 @@
+<?php
+$x = 5;
+$txt = "Hello world!";
+$y = 10.5;
+
+echo $txt . "<br>";
+echo "Nilai x = " . $x . "<br>";
+echo "Nilai y = " . $y;
+?>
---
+++ Modul1/22-187_Wahyu Ifandi_TP1/Soal6.php
@@ -0,0 +1,4 @@
+<?php
+$txt = "Dan Badan";
+echo "I love $txt!";
+?>
