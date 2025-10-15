4ed43ed1947318a139ce98291ee61d833f9a281c
FendiEka <160805386+FendiEka@users.noreply.github.com>
2025-10-15 22:24:29 +0700

    Add files via upload

---
+++ Modul3/22-049_Fendi_Eka_Saputra_TP_3/3.3.3.php
@@ -0,0 +1,8 @@
+<?php
+  $weight = array("Andy"=>"65","Barry"=>"70","Charlie"=>"60");
+
+  echo "Data ke-2 dari array \$weight: ";
+  $values = array_values($weight); 
+  echo $values[1]." kg"; 
+
+?>
---
+++ Modul3/22-049_Fendi_Eka_Saputra_TP_3/313.php
@@ -0,0 +1,5 @@
+<?php
+ $veggies = array("Carrot", "Broccoli", "Spinach");
+   echo "Seluruh data dari array \$veggies:<br>";
+   var_dump($veggies);
+?>
