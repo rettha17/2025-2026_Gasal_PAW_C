3cb905c990bbf9c2f1a5d111098efc39b35408ff
rettha17 <rettha760@gmail.com>
2025-10-24 19:38:31 +0700

    Create 3.4.php

---
+++ Modul3/23-072_Cintya Margareth Sihombing/3.4.php
@@ -0,0 +1,8 @@
+<?php
+$height = array("Andy" => "176", "Barry" => "165", "Charlie" => "170");
+
+foreach($height as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+?>
