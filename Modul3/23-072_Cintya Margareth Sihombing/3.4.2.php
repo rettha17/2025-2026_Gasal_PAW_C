f987f22f5b1e0488a98f30fd50d1c49b6266f9d5
rettha17 <rettha760@gmail.com>
2025-10-24 19:39:33 +0700

    Add PHP script to display names and weights

---
+++ Modul3/23-072_Cintya Margareth Sihombing/3.4.2.php
@@ -0,0 +1,12 @@
+<?php
+$sweight = array(
+  "David" => "75",
+  "Ella" => "55",
+  "Fiona" => "62"
+);
+
+foreach($sweight as $x => $x_value) {
+  echo "Key = " . $x . ", Value = " . $x_value;
+  echo "<br>";
+}
+?>
