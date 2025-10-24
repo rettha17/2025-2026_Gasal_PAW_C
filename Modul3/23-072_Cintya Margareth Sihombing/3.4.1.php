186cb4be6ae06d17ba84565f49aea2859e5669d8
rettha17 <rettha760@gmail.com>
2025-10-24 19:39:02 +0700

    Add PHP script to display heights of individuals

---
+++ Modul3/23-072_Cintya Margareth Sihombing/3.4.1.php
@@ -0,0 +1,17 @@
+<?php
+$height = array(
+  "Andy" => "176",
+  "Barry" => "165",
+  "Charlie" => "170",
+  "David" => "180",
+  "Ella" => "160",
+  "Fiona" => "168",
+  "George" => "172",
+  "Helen" => "158"
+);
+
+foreach($height as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+?>
