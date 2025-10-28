bd85f954ae894b6cc6d680310d9d2eb1bd0be02a
ErlanggaRubai <erlanggarh13@gmail.com>
2025-10-28 22:50:52 +0700

    Add files via upload

---
+++ Modul4/22-051_Erlangga_Rubai_Hikmatulloh/2/ProcessData.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Posted Data : </h3>
+    <?php require 'validate.inc';
+    $errors = array();
+    validateName($errors, $_POST, 'surname');
+    if ($errors) {
+        echo 'Errors:<br />';
+        foreach ($errors as $field => $error)
+            echo "$field $error</br>";
+    } else {
+        echo 'Data OK!';
+    }
+    ?>
+</body>
+
+</html>
